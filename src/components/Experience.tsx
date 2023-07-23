1b2c3d4cff10d96545bc5182a85ebde741dad1db
BillarySquintin <BillarySquintin@gmail.com>
2023-07-23 15:24:51 -0700

    🏀 fun lil basketball game

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,6 +7,13 @@ import {useRef, useState} from "react";
 import * as THREE from 'three';
 
 export const Experience = () => {
+const [hover, setHover] = useState(false);
+
+const ballman=useRef<RapierRigidBody>(null) ;
+const jump = () => {
+  ballman.current?.applyImpulse({x: 0,y: 3,z: 0}, true)
+}
+
 const hoop=useRef<RapierRigidBody>(null) ;
 
 useFrame((_state, delta) => {
@@ -14,7 +21,7 @@ useFrame((_state, delta) => {
       const curRotation = quat(hoop.current.rotation());
       const incrementRotation = new THREE.Quaternion().setFromAxisAngle(
         new THREE.Vector3(0,1,0),
-        delta * 2
+        delta * 1.5
       );
       curRotation.multiply(incrementRotation);
       hoop.current.setNextKinematicRotation(curRotation)
@@ -28,18 +35,23 @@ useFrame((_state, delta) => {
     <directionalLight position={[10,-10,0]} intensity={0.4}/>
       <OrbitControls />
       
-      <RigidBody colliders="trimesh" position={[1,0,0]} type='kinematicPosition' ref={hoop}>
+      <RigidBody colliders="trimesh" position={[0,0,0]} type='kinematicPosition' ref={hoop}>
         <Box args={[3,0.5,0.5]} position={[1.5,.5,0]}>
           <meshStandardMaterial color='hotpink'/>
         </Box>
 
-        <Torus position={[3.5,1,0]} rotation={[1.5,0,0]} scale={0.8} >
+        <Torus position={[3.5,1,0]} rotation={[1.5,0,0]} scale={0.9} >
           <meshStandardMaterial color='royalblue' />
         </Torus>
       </RigidBody>
 
-      <RigidBody colliders='ball'>
-        <Sphere position={[3,3,0]} >
+      <RigidBody colliders='ball' ref={ballman}>
+        <Sphere args={[.5]} position={[3.5,5,0]}  
+        onPointerEnter={() => setHover(true)} 
+        onPointerLeave={() => setHover(false)}
+        onClick={jump}
+        >
+        <meshStandardMaterial color={hover ? 'magenta' : "orange"} />
         </Sphere>
       </RigidBody>
 
